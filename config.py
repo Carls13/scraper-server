5908786c7919b6ce30d3bc584e7a0e4eb603f904
Carls13 <charlesshb98@gmail.com>
2020-06-09 14:13:48 -0400

    Configs

---
+++ config.py
@@ -0,0 +1,15 @@
+from decouple import config
+
+class Config:
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
