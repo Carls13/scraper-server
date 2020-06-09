e8612f8c1c1e858043dfe1151d8cd96d7893d47a
Carls13 <charlesshb98@gmail.com>
2020-06-09 14:17:04 -0400

    Removing config

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from jobs_scraper import scrape_all
 
 app = Flask(__name__)
 
-enviroment = config['development']
+# enviroment = config['development']
 
-if config_decouple('PRODUCTION', default=False):
-    enviroment = config['production']
+# if config_decouple('PRODUCTION', default=False):
+#     enviroment = config['production']
 
-app.config.from_object(enviroment)
+# app.config.from_object(enviroment)
 
 @app.route('/hacker-news', methods=['GET'])
 def get_news():
